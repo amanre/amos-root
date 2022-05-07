1929ec97c0089464198c23e0f59a5a4475323a24
amanre <amanre556@gmail.com>
2022-05-07 13:35:53 +0200

    update

--- etc/skel/.conky/conky-pywal/start_conky.sh
+++ etc/skel/.conky/conky-pywal/start_conky.sh
@@ -5,5 +5,5 @@ conky -c ~/.conky/conky-pywal/configs/ram.conf&
 conky -c ~/.conky/conky-pywal/configs/ssd.conf&
 conky -c ~/.conky/conky-pywal/configs/clock.conf&
 conky -c ~/.conky/conky-pywal/configs/gpu.conf&
-conky -c ~/.conky/conky-pywal/configs/battery.conf&
+#conky -c ~/.conky/conky-pywal/configs/battery.conf&
 while inotifywait -qqe modify $HOME/.cache/wal/colors.Xresources; do ~/.conky/conky-pywal/refresh_conky.sh ; done
